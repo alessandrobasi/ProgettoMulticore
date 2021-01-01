841b4644426199b9025492846676e15015944d55
alessandrobasi <13107787+alessandrobasi@users.noreply.github.com>
2021-01-01 20:11:14 +0100

    init omp

--- ProgettoMulticore.cpp
+++ ProgettoMulticore.cpp
@@ -11,24 +11,59 @@ Studenti:
 Sorgenti:
     https://en.wikipedia.org/wiki/Sudoku_solving_algorithms
     http://www.ams.org/notices/200904/tx090400460p.pdf
-    https://ichi.pro/it/risolvi-il-sudoku-in-modo-piu-elegante-con-l-algoritmo-di-crook-in-python-105010293053459
+    https://docs.microsoft.com/it-it/cpp/parallel/openmp/openmp-in-visual-cpp?view=msvc-160
 
 */
 
 #include <iostream>
+#include <vector>
+#include <array>
 #include <stdio.h>
 #include <omp.h>  // Impostazioni di Visual Studio | Proprietà -> C/c++ -> Linguaggio -> Supporto per OpenMP
-#include <mpi.h>  // https://www.microsoft.com/en-us/download/details.aspx?id=100593
+
+typedef std::array<int, 81> sudo;
+typedef std::vector<std::vector<std::vector<int>>> sudonote;
+
+sudo solving(sudo sudoku) {
+    int my_rank = omp_get_thread_num();
+    //int thread_count = omp_get_num_threads();
+#pragma omp critical
+    std::cout << my_rank << "/" << thread_count << std::endl;
+}
 
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello World!" << std::endl;
-
-    std::cout << "Parametri: ";
-
-    for (int i = 1; i < argc; i++) {
-        printf("%s ", argv[i]);
+    
+    if (argc <= 1) {
+        std::cout << "\nInserire come argomento al programma il numero di thread\n" << argv[0] << " <Num thread>\n\n" << std::endl;
+        return 0;
     }
+    int thread_count = atoi(argv[1]);
+
+    sudo sudoku = {
+             0 , 0 , 0 , 1 , 8 , 0 , 3 , 6 , 0 , //  0 ...  8
+             0 , 7 , 0 , 2 , 0 , 4 , 0 , 0 , 8 , //  9 ... 17
+             0 , 0 , 0 , 0 , 5 , 7 , 2 , 0 , 9 , // 18 ... 26
+             1 , 6 , 0 , 0 , 7 , 0 , 9 , 8 , 0 , // 27 ... 35
+             8 , 0 , 3 , 5 , 0 , 1 , 6 , 0 , 2 , // 36 ... 44
+             0 , 5 , 7 , 0 , 9 , 0 , 0 , 3 , 4 , // 45 ... 53
+             5 , 0 , 2 , 7 , 6 , 0 , 0 , 0 , 0 , // 54 ... 62
+             9 , 0 , 0 , 4 , 0 , 8 , 0 , 5 , 0 , // 63 ... 71
+             0 , 8 , 6 , 0 , 1 , 5 , 0 , 0 , 0 , // 72 ... 80
+    };
+
+
+    #pragma omp parallel num_threads(thread_count)
+    {
+        int my_rank = omp_get_thread_num();
+        //int thread_count = omp_get_num_threads();
+        #pragma omp critical
+        std::cout << my_rank << "/" << thread_count << std::endl;
+    }
+    
+    #pragma omp parallel num_threads(thread_count) 
+    solving(sudoku);
+
 
     return 0;
 }
