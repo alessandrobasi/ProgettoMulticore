1f25a92de3495927dfd8459b0bda9b4ee17fc288
alessandrobasi <13107787+alessandrobasi@users.noreply.github.com>
2021-02-02 11:45:34 +0100

    Update ProgettoMulticore.cpp

--- ProgettoMulticore.cpp
+++ ProgettoMulticore.cpp
@@ -26,8 +26,8 @@ Sorgenti:
 #include <iterator>
 #include <array> 
 #include <stdio.h>
-//#include <omp.h>  // Impostazioni di Visual Studio | Proprietà -> C/c++ -> Linguaggio -> Supporto per OpenMP
-#include "mpi.h"  // https://www.microsoft.com/en-us/download/details.aspx?id=100593
+#include "mpi.h"
+// https://www.microsoft.com/en-us/download/details.aspx?id=100593
 
 #define DEBUG false
 #define MAX_BUFF 81
@@ -37,37 +37,65 @@ typedef std::array<int, 81> sudo;
 typedef std::vector<std::vector<std::vector<int>>> sudonote;
 
 
-bool check_row(sudo sudoku, int trynum, int riga) {
-
-    for (int incr = 0; incr < 9; incr++) {
-        if (trynum == sudoku[riga*9 + incr]) {
-            return true;
+/////////////////////////////
+/*
+questo gruppo di funzioni controllano nel sudoku passato se rispettivamente,
+in riga, colonna o quadratro esiste o meno il numero passato,
+se esiste ritorna true, altrimentri false
+*/
+bool _check_row(sudo sudoku, int trynum, int riga) {
+    bool test = false;
+    int incr;
+    for (incr = 0; incr < 9; incr++) {
+        if (!test && trynum == sudoku[riga * 9 + incr]) {
+            test = true;
         }
     }
-    return false;
+    return test;
 }
 
-bool check_col(sudo sudoku, int trynum, int col) {
-    for (int incr = 0; incr < 9; incr++) {
-        if (trynum == sudoku[col + 9 * incr]) {
-            return true;
+bool _check_col(sudo sudoku, int trynum, int col) {
+    bool test = false;
+    int incr;
+    for (incr = 0; incr < 9; incr++) {
+        if (!test && trynum == sudoku[col + 9 * incr]) {
+            test = true;
         }
     }
-    return false;
+    return test;
 }
 
-bool check_square(sudo sudoku, int trynum, int riga, int col) {
+bool _check_square(sudo sudoku, int trynum, int riga, int col) {
     riga /= 3;
     col /= 3;
-
-    for (int add_riga = 0; add_riga < 3; add_riga++) {
-        for (int add_col = 0; add_col < 3; add_col++) {
-            if (trynum == sudoku[ 9*(3*riga + add_riga) + (3*col + add_col) ]) {
-                return true;
+    bool test = false;
+    int add_riga, add_col;
+    for (add_riga = 0; add_riga < 3; add_riga++) {
+        for (add_col = 0; add_col < 3; add_col++) {
+            if (!test && trynum == sudoku[9 * (3 * riga + add_riga) + (3 * col + add_col)]) {
+                test = true;
             }
         }
     }
-    return false;
+    return test;
+}
+/////////////////////////////
+
+/*
+questa funzione raggruppa i risultati del gruppo di verifica del numero
+ritorna fasle se almeno una funzione ritorna true
+*/
+bool check_space(sudo sudoku, int trynum, int row, int col) {
+
+    bool Brow, Bcol, Bsquare;
+
+    Brow = _check_row(sudoku, trynum, row);
+    Bcol = _check_col(sudoku, trynum, col);
+    Bsquare = _check_square(sudoku, trynum, row, col);
+
+    // le funzioni ritornano se il numero ? presente nella riga|colonna|quadrato
+    // di conseguenza se ? presente il posto non va bene
+    return !Brow && !Bcol && !Bsquare;
 }
 
 /*
@@ -103,9 +131,7 @@ void markup(sudo sudoku, int casella, sudonote &note) {
 
     for (int trynum = 1; trynum <= 9; trynum++) {
 
-        if (!check_row(sudoku, trynum, riga) && 
-            !check_col(sudoku, trynum, colonna) && 
-            !check_square(sudoku, trynum, riga, colonna)) {
+        if (check_space(sudoku, trynum, riga, colonna)) {
 
             note[riga][colonna].push_back(trynum);
             
@@ -187,6 +213,7 @@ int main(int argc, char* argv[]) {
 
     // griglia vuota di note
     sudonote note;
+    sudonote recv_note;
 
     // definisco la grandezza del numero di righe e colonne
     note.resize(9);
@@ -251,19 +278,36 @@ int main(int argc, char* argv[]) {
     // Faccio il taglio dall'inizio del lavoro del thread alla fine
     std::copy(sudoku.begin() + inizio, sudoku.begin() + fine, sudoku.begin());
 
-
     // A questo punto i thread devono scambiarsi le proprie tabelle con solo le caselle lavorate
-    //MPI_Allgather(&sudoku, fine - inizio, MPI_INT, &recv_sudoku, fine - inizio, MPI_INT, MPI_COMM_WORLD);
-    MPI_Gather(&sudoku, fine - inizio, MPI_INT, &recv_sudoku, fine - inizio + (81 % com_size), MPI_INT, ROOT, MPI_COMM_WORLD);
+    MPI_Allgather(&sudoku, lavoro_per_thread, MPI_INT, &recv_sudoku, lavoro_per_thread, MPI_INT, MPI_COMM_WORLD);
+    //MPI_Gather(&sudoku, lavoro_per_thread, MPI_INT, &recv_sudoku, lavoro_per_thread, MPI_INT, ROOT, MPI_COMM_WORLD);
 
     if (rank == ROOT) {
         _pprint(recv_sudoku);
         sudoku = recv_sudoku;
     }
-
-
+    // tutti i thread hanno un sudoku risolto dai singleton
     MPI_Barrier(MPI_COMM_WORLD);
+
     
+
+    std::copy(note.data()->begin() + inizio, note.data()->begin() + fine, note.data()->begin());
+
+    // A questo punto i thread devono scambiarsi le proprie tabelle con solo le caselle lavorate
+    MPI_Allgather(&note, lavoro_per_thread, MPI_INT, &recv_note, lavoro_per_thread, MPI_INT, MPI_COMM_WORLD);
+    //MPI_Gather(&sudoku, lavoro_per_thread, MPI_INT, &recv_sudoku, lavoro_per_thread, MPI_INT, ROOT, MPI_COMM_WORLD);
+
+    if (rank == ROOT) {
+        _pprint(recv_note);
+        note = recv_note;
+    }
+    // tutti i thread hanno un sudoku risolto dai singleton
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
+
+
+
     // PASSO 3 di Crook
 
 
